49728f9fdd33f7e0ae567744fd69df6c4ecc82e8
Alexey <alx.did.00@gmail.com>
2019-09-08 16:40:01 +0300

    OS Program
    OS Program

---
+++ Process.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processes
+{
+    class Process
+    {
+        private string name;
+        private int basep;
+        private int curp;
+        private int acticks;
+
+        public int BasePublic
+        {
+            get
+            {
+                return basep;
+            }
+        }
+        public int CurPublic
+        {
+            get
+            {
+                return curp;
+            }
+            set
+            {
+                curp = value;
+            }
+        }
+
+        public Process(string n, int bp)
+        {
+            this.name = n;
+            this.basep = bp;
+        }
+    }
+}
---
+++ ProcessManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processes
+{
+    class ProcessManager
+    {
+        private List<Process> pr;
+
+        public void AddProcess(string name, int basepr)
+        {
+            pr.Add(new Process(name, basepr));
+        }
+
+        public void Transposition()
+        {
+            int max = 0;
+            int indxb = 0;
+            foreach (Process prio in pr)
+            {
+                if (prio.BasePublic > max)
+                {
+                    max = prio.BasePublic;
+                    indxb = pr.IndexOf(prio);
+                }
+            }
+            foreach (Process prior in pr)
+            {
+                prior.CurPublic = prior.BasePublic + 1;
+                //pr.Insert(indxb, max); Как в текущий приоритет положить на этот же процесс поставить максимальный базовый приоритет?
+            }
+
+        }
+    }
+}
